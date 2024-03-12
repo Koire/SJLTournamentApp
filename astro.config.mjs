eda0680a690c3da35f26aa341325408989a97f4b
koire <pikewb@gmail.com>
2024-03-12 09:24:10 +0900

    fix: remove deno for the server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import deno from '@astrojs/deno'
+// import deno from '@astrojs/deno'
 // https://astro.build/config
-export default defineConfig({
-    output: 'server',
-    adapter: deno()
-});
+export default defineConfig();
