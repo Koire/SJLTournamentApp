aa72fd484b97fe0c78e3ec4c5e1fbd2df52e327f
Koire <pikewb@gmail.com>
2024-04-08 03:36:34 +0000

    fix: hopefully fixed getting ability data

--- tools/scrapeData.js
+++ tools/scrapeData.js
@@ -11,8 +11,18 @@ const scrapePage = async () => {
     await page.goto("https://marvelsnapzone.com/cards/")
     const cardData = await page.evaluate(() => {
         const cards = document.querySelectorAll(".simple-card")
+        return Array.from(cards).map(card => {
+            if(card.dataset.ability.length === 0){
+                const missingCardDetails = document
+                .querySelector(`[data-carddefid="${card.dataset.carddefid}]`)
+                .querySelector('.card-description').textContent
+            }
 
-        return Array.from(cards).map(card => ({...card.dataset})) 
+            return { 
+                ...card.dataset,
+                ability: card.dataset.ability || missingCardDetails
+            }
+        }) 
     })
     await fs.writeFile("resources/cardData.json", JSON.stringify(cardData, null, 4))
 
